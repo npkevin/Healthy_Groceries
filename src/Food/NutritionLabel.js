d309918c60569003e886cfa19eb77d50275c771f
Kevin Nguyen <kevnguyenp@gmail.com>
2021-04-12 14:43:38 -0400

    Completed dynamic nutritional dailyvalues

--- src/Food/NutritionLabel.js
+++ src/Food/NutritionLabel.js
@@ -24,34 +24,64 @@ class NutritionLabel extends Component {
     this.setState({ serves: newVal })
   }
 
-  // Sum/Merge all the nurients together
-  getAllNutrients = (foodArr) => {
-    let tmpNutrients = {}
-    foodArr.forEach(food => {
-      const nutrients = (food.user ? food.user.nutrients : food.nutrients)
-      Object.keys(nutrients).forEach(n => {
-        if (tmpNutrients.hasOwnProperty(n)) {
-          tmpNutrients[n] += nutrients[n]
-        } else {
-          tmpNutrients[n] = nutrients[n]
-        }
-      })
-    })
-    return tmpNutrients
-  }
-
   // 9cals/fat, 4cals/prot, 4cals/carbs
   getCalories = (calculatedNutrients) => {
     return Math.ceil(
       calculatedNutrients.totalFats * 9 +
       calculatedNutrients.totalProtiens * 4 +
       calculatedNutrients.totalCarbs * 4
-    );
+      );
+    }
+
+    // Sum/Merge all the nurients together
+    getAllNutrients = (foodArr) => {
+      let tmpNutrients = {}
+      foodArr.forEach(food => {
+        const nutrients = food.user ? food.user.nutrients : food.nutrients
+        Object.keys(nutrients).forEach(n => {
+          if (tmpNutrients.hasOwnProperty(n)) {
+            tmpNutrients[n] += nutrients[n]
+          } else {
+            tmpNutrients[n] = nutrients[n]
+          }
+        })
+      })
+      return tmpNutrients
+    }
+    
+  getDailyValue = (foodArr) => {
+    let tmpNutrients = {
+      totalFats: 0,
+      totalCarbs: 0,
+      sattransfat_g: 0,
+      cholesterol_mg: 0,
+      sodium_mg: 0,
+      fiber_g: 0,
+      vataminA_re: 0,
+      vitaminC_mg: 0,
+      calcium_mg: 0,
+      iron_mg: 0,
+    }
+    foodArr.forEach(food => {
+      const nutrients = food.user ? food.user.nutrients : food.nutrients
+      tmpNutrients.totalFats += nutrients.totalFats / 65
+      tmpNutrients.totalCarbs += nutrients.totalCarbs / 300
+      tmpNutrients.sattransfat_g += (nutrients.fattyAcids_satur_g + nutrients.fattyAcids_trans_g) / 20
+      tmpNutrients.cholesterol_mg += nutrients.cholesterol_mg / 300
+      tmpNutrients.sodium_mg += nutrients.sodium_mg / 2400
+      tmpNutrients.fiber_g += nutrients.fiber_g / 25
+      tmpNutrients.vataminA_re += nutrients.vitaminA_RAE_ug / 1000 // 1 ug RAE = 1 ug RE
+      tmpNutrients.vitaminC_mg += nutrients.vitaminC_mg / 60
+      tmpNutrients.calcium_mg += nutrients.calcium_mg / 1100
+      tmpNutrients.iron_mg += nutrients.iron_mg / 14
+    })
+    console.log(tmpNutrients)
+    return tmpNutrients
   }
 
   HR = (props) => {
-    const size = props.thin? "HR--thin " : props.thick? "HR--thick " : ""
-    const light = props.light? "light" : ""
+    const size = props.thin ? "HR--thin " : props.thick ? "HR--thick " : ""
+    const light = props.light ? "light " : ""
     return (
       <div className={"HR " + size + light} />
     )
@@ -61,18 +91,18 @@ class NutritionLabel extends Component {
   Nutrient = (props) => {
     return (
       <span className={"nutrient " + (props.className ? props.className : "")}>
-        <span style={props.bold ? {fontWeight: "bold"} : null}>{props.name} </span>
+        <span style={props.bold ? { fontWeight: "bold" } : null}>{props.name} </span>
         <span>{props.value ? Math.ceil(props.value / this.state.serves) : 0} {props.unit}</span>
-        {props.dailyValue ? <span className="dpercent">{props.dailyValue} %</span> : null}
+        {props.dailyValue ? <span className="dpercent">{Math.ceil((props.dailyValue / this.state.serves) * 100)}%</span> : null}
       </span>
     )
   }
 
   render = () => {
-
     const HR = this.HR
     const Nutrient = this.Nutrient
     const calculatedNutrients = this.getAllNutrients(this.state.foods)
+    const calculatedDailyValue = this.getDailyValue(this.state.foods)
 
     // Eng/Fre translations
     const tittle_phrase = this.state.lang_fr ? "Valeur nutritive" : "Nutrition Facts"
@@ -117,13 +147,13 @@ class NutritionLabel extends Component {
 
         {/* ===== FATS START ===== */}
         <div className="NL__totalFats">
-          <Nutrient bold name={fat_phrase} unit="g" value={calculatedNutrients.totalFats} dailyValue="##" />
+          <Nutrient bold name={fat_phrase} unit="g" value={calculatedNutrients.totalFats} dailyValue={calculatedDailyValue.totalFats} />
           <div className="breakdown">
             <div className="breakdown__values">
               <Nutrient name={satur_phrase} unit="g" value={calculatedNutrients.fattyAcids_satur_g} />
-              <Nutrient name={trans_phrase} unit="g" value={calculatedNutrients.fattyAcids_trans_g}/>
+              <Nutrient name={trans_phrase} unit="g" value={calculatedNutrients.fattyAcids_trans_g} />
             </div>
-            <span className="dpercent">## %</span>
+            <span className="dpercent">{Math.ceil((calculatedDailyValue.sattransfat_g / this.state.serves) * 100)}%</span>
           </div>
         </div>
         {/* ===== FATS END ===== */}
@@ -133,7 +163,7 @@ class NutritionLabel extends Component {
 
         {/* ===== CARBS START ===== */}
         <div className="NL__totalCarbs">
-          <Nutrient bold name={carb_phrase} unit="g" value={calculatedNutrients.totalCarbs} dailyValue="##" />
+          <Nutrient bold name={carb_phrase} unit="g" value={calculatedNutrients.totalCarbs} dailyValue={calculatedDailyValue.totalCarbs} />
           <div className="breakdown">
             <div className="breakdown__values">
               <Nutrient name={fibre_phrase} unit="g" value={calculatedNutrients.fiber_g} />
@@ -144,37 +174,37 @@ class NutritionLabel extends Component {
         {/* ===== CARBS END ===== */}
         <HR thin />
 
-        <Nutrient className="NL__totalProtiens" bold name={protien_phrase} unit="g" value={calculatedNutrients.totalProtiens}/>
+        <Nutrient className="NL__totalProtiens" bold name={protien_phrase} unit="g" value={calculatedNutrients.totalProtiens} />
         <HR thin />
-        <Nutrient bold name={cholest_phrase} unit="mg" value={calculatedNutrients.cholesterol_mg}/>
+        <Nutrient bold name={cholest_phrase} unit="mg" value={calculatedNutrients.cholesterol_mg} dailyValue={calculatedDailyValue.cholesterol_mg} />
         <HR thin />
-        <Nutrient bold name="Sodium" unit="mg" value={calculatedNutrients.sodium_mg}/>
+        <Nutrient bold name="Sodium" unit="mg" value={calculatedNutrients.sodium_mg} dailyValue={calculatedDailyValue.sodium_mg} />
         <HR thick />
 
-        
+
         {/* Nurtrients of Public Intrest */}
-        <Nutrient name="Potassium" unit="mg" value={calculatedNutrients.potassium_mg}/>
+        <Nutrient name="Potassium" unit="mg" value={calculatedNutrients.potassium_mg} />
         <HR thin />
-        <Nutrient name="Calcium" unit="mg" value={calculatedNutrients.calcium_mg}/>
+        <Nutrient name="Calcium" unit="mg" value={calculatedNutrients.calcium_mg} dailyValue={calculatedDailyValue.calcium_mg} />
         <HR thin />
-        <Nutrient name={iron_phrase} unit="mg" value={calculatedNutrients.iron_mg}/>
+        <Nutrient name={iron_phrase} unit="mg" value={calculatedNutrients.iron_mg} dailyValue={calculatedDailyValue.iron_mg} />
         <HR thick />
 
         {/* Daily Value Footnote */}
         {this.state.lang_fr ?
-        <span style={{ fontSize: "0.8rem" }}>
-          <span style={{ fontWeight: "bold" }}>*</span> 5% ou moins c'est
+          <span style={{ fontSize: "0.8rem" }}>
+            <span style={{ fontWeight: "bold" }}>*</span> 5% ou moins c'est
           <span style={{ fontWeight: "bold" }}> peu</span>, 15% ou plus c'est
           <span style={{ fontWeight: "bold" }}> beaucoup</span>
-        </span>
-        :
-        <span style={{ fontSize: "0.8rem" }}>
-          <span style={{ fontWeight: "bold" }}>*</span> 5% or less is
+          </span>
+          :
+          <span style={{ fontSize: "0.8rem" }}>
+            <span style={{ fontWeight: "bold" }}>*</span> 5% or less is
           <span style={{ fontWeight: "bold" }}> a little</span>, 15% or more is
           <span style={{ fontWeight: "bold" }}> a lot</span>
-        </span>
+          </span>
         }
-        
+
       </div>
     )
   }
