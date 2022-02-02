3cab7a92a1267237c2b98b59a670b5770014fc57
Kevin Nguyen <kevnguyenp@gmail.com>
2022-02-02 12:25:08 -0500

    CSS/SASS Improvements

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import firebase from 'firebase/app'
 import './App.css';
 
 import RecipeView from './Recipe/RecipeView'
-
 import config from './firebase_config';
+
 if (firebase.apps.length === 0) firebase.initializeApp(config);
 
 class App extends Component {
